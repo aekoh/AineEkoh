a9425e961a43bc399f5967a1d8d3ac47a6c4ad86
aine <aineekohh@yahoo.com>
2019-07-12 20:28:31 -0500

    created assets folder in public folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+//import Main from "./Components/Main";
+
+var App = () => {
+  return (
+    <div className="App" id="main">
+      
+    </div>
+  );
+};
+
+export default App;
